4ea74c16b6043e8171cc7e4fc7cf7812ae49604d
mengerwei521 <mengerwei521@163.com>
2022-05-08 22:25:25 +0800

    main11

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 <Script>
-<<<<<<< HEAD
-let main = 'main';
-    let dev ='main';
-    let meng ='main'
-</Script>
-=======
-    let main = 'main';
-    let dev ='dev';
-    let meng ='meng'
+    let main = 'main1';
+    let dev ='dev1';
+    let meng ='meng1'
 </Script> 
->>>>>>> badec727c96dccefb57dec6d818314ea015a9951
