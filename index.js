9d9fa57e6a041e5060ad0c20a66694d4ae08eef0
mengerwei521 <mengerwei521@163.com>
2022-05-10 22:58:52 +0800

    1

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 <Script>
     let main = 'main11';
-<<<<<<< HEAD
-    let dev ='main11';
-    let meng ='meng11';
-=======
     let dev ='dev11';
     let meng ='main11';
->>>>>>> 8b9ca05588ddf713dfea76da346e18a4df187e09
 </Script> 
