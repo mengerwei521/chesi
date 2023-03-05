29a54ca49390dc2211583139c07f959466f187a2
mengerwei521 <mengerwei521@163.com>
2023-03-05 17:03:16 +0800

    dev-sss

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
     let dev ='dev11';
     let meng ='cccccccccc';
     if(dev == '1') {
-        let url = 'www.baidu.com'+'?meng=2';
+        let url = 'www.baidu.comsssss'+'?meng=2';
     }
 </Script> 
