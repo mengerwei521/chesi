b57805a5dec0bbfc463cb533e36fd11e323813db
mengerwei521 <mengerwei521@163.com>
2023-03-06 13:51:58 +0800

    分支main-ulr

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
     let dev ='dev11';
     let meng ='cccccccccc';
     if(dev == '1') {
-        let url = 'www.baidu.com'+'?meng=2';
+        let url-main = 'www.baidu.com'+'?meng=2';
     }
 </Script> 
