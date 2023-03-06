cd809507209007a80ad1d999363b1f3a29e2dc3c
mengerwei521 <mengerwei521@163.com>
2023-03-06 17:58:35 +0800

    分支-d

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
     let dev-dev ='dev11';
     let meng ='cccccccccc';
     if(dev == '1') {
-        let url = 'www.baidu.com'+'?meng=2';
+        let url-d = 'www.baidu.com'+'?meng=2';
     }
 </Script> 
