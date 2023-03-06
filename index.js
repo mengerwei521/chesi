8c45ee385d671b9e244f606104d6b984d1f76413
mengerwei521 <mengerwei521@163.com>
2023-03-06 11:14:09 +0800

    分支dev

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 <Script>
     let main = 'main222';
-    let dev ='dev11';
+    let dev-dev ='dev11';
     let meng ='cccccccccc';
     if(dev == '1') {
         let url = 'www.baidu.com'+'?meng=2';
