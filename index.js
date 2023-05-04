7b7e9cca1cbfdb5bbd94b4824d214a484d268e1a
mengerwei521 <mengerwei521@163.com>
2023-05-04 18:58:18 +0800

    main

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 <Script>
-    let main-main = 'main222';
-    let dev ='dev11';
-    let meng ='cccccccccc';
-    if(dev == '1') {
-        let url-main = 'www.baidu.com'+'?meng=2';
-    }
+    let main ='111'
 </Script> 
